9901b6d5fca7c2928607bd89dd6ff03320d58199
Ivailo02 <makgirata@gmail.com>
2022-03-29 09:43:06 +0300

    Add Dentalclinic ApplicationUser

---
+++ DentalClinicApp/DentalClinic/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DentalClinic.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DentalClinic.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
